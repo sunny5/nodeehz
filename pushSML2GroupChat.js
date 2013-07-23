f1c21ed4da92f8f35a14cd651a01f28528948530
root <root@Sunny5>
2013-07-23 15:24:43 +0200

    Fixed pushSML2GroupChat integer sign prob on 15_7_0

--- pushSML2GroupChat.js
+++ pushSML2GroupChat.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 //*** Continuously read SML data from EHZ and push OBIS value 15.7.0 (Wirkleistung)
 //*** to xmpp server room.
 
-objConfig = JSON.parse(fs.readFileSync('./grid-config.json', 'utf8'));
+objConfig = JSON.parse(fs.readFileSync('/home/sunny5/nodeehz/grid-config.json', 'utf8'));
 strGroupchatServer = objConfig["groupchat-server"];
 intGroupchatPort = objConfig["groupchat-port"];
 strGroupchatRoom = objConfig["groupchat-room"];
@@ -37,7 +37,7 @@ var reg15_7_0 = new RegExp('0701000f0700.{16}(.{8})0177');
 var reg1_8_0 = new RegExp('070100010800.{24}(.{8})0177');
 var reg2_8_0 = new RegExp('070100020800.{24}(.{8})0177');
 var arr15_7_0, arr1_8_0, arr2_8_0;
-var int1_8_0=0, int1_8_0prev="0", int2_8_0=0, int15_7_0;
+int1_8_0=0, int2_8_0prev=0, int2_8_0=0, int15_7_0=0;
 var chunk = "";
 
 serial_port.on("data", function (data) {
@@ -52,29 +52,29 @@ serial_port.on("data", function (data) {
         int15_7_0 = parseInt(arr15_7_0[1],16)/10;
         int1_8_0 = parseInt(arr1_8_0[1],16)/10000;
         int2_8_0 = parseInt(arr2_8_0[1],16)/10000;
-      
-        if (arr1_8_0[1] > int1_8_0prev) {
-           int1_8_0prev = arr1_8_0[1];
+
+	if (arr2_8_0[1] <= int2_8_0prev) {
+           int2_8_0prev = arr2_8_0[1];
         }
         else {
-           int1_8_0prev = arr1_8_0[1];
+           int2_8_0prev = arr2_8_0[1];
            int15_7_0 = int15_7_0*-1;
         }
         bolSend = true;
+
+        console.log('---new data---');
+        console.log("Wirkleistung momentan (15_7_0): " + int15_7_0  + " W");
+        console.log("Gesamt Bezug (1_8_0): " + int1_8_0 + " kW");
+        console.log("Gesamt Einspeisung (2_8_0): " + int2_8_0 + " kW");
+        //console.log(chunk);
+
+        if (conn.JOINEDROOM == true && bolSend == true) {
+            conn.send("<message from='"+strGroupchatRoom+"/ehz' id='mymsg' to='"+strGroupchatRoom+"' type='groupchat'><body>e:"+Math.round(int15_7_0)+"</body></message>");
+            console.log("Groupchat message sent.");
+        }
+
+        chunk = "";
       }
-
-      console.log('---new data---');
-      console.log("Wirkleistung momentan: " + int15_7_0  + " W");
-      console.log("Gesamt Bezug: " + int1_8_0 + " kW");
-      console.log("Gesamt Einspeisung: " + int2_8_0 + " kW");
-      //console.log(chunk);
-
-      if (conn.JOINEDROOM == true && bolSend == true) {
-          conn.send("<message from='"+strGroupchatRoom+"/ehz' id='mymsg' to='"+strGroupchatRoom+"' type='groupchat'><body>e:"+Math.round(parseInt(arr15_7_0[1],16)/10)+"</body></message>");
-          console.log("Groupchat message sent.");
-       }
-
-      chunk = "";
    }
    if (chunk.length > 5000) {
       //avoid memory leak on unknown/unmatched ehz
